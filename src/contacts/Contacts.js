03a39d8524620760199fd63d9377f21878123195
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-12 21:28:11 +0300

    correct contact form

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -2,7 +2,6 @@ import React from "react";
 import style from './Contacts.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 
-
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
@@ -10,11 +9,38 @@ export const Contacts = () => {
                 <span>Contact</span>
                 <h2 className={style.title}>Contact With Me</h2>
                 <form className={style.contactsForm}>
-                    <input type={'text'}/>
-                    <input type={'text'}/>
-                    <textarea/>
+                    <div className={style.blockInfo}>
+                        <div>
+                            <label>Your name</label>
+                            <span>
+                                 <input type={'text'}/>
+                            </span>
+                        </div>
+                    </div>
+                    <div className={style.blockInfo}>
+                        <div>
+                            <label>Phone number</label>
+                            <span>
+                                 <input type={'text'}/>
+                            </span>
+                        </div>
+                    </div>
+                    <div className={style.blockInfo}>
+                        <div>
+                            <label>Email</label>
+                            <span>
+                                 <input type={'text'}/>
+                            </span>
+                        </div>
+                    </div>
+                    <div className={style.blockInfo}>
+                        <div>
+                            <label>Your message</label>
+                            <textarea/>
+                        </div>
+                    </div>
                 </form>
-                <button className={style.button}>Send</button>
+                <button className={style.button}>Send message</button>
             </div>
         </div>
     );
