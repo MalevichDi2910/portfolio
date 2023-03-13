1093039ee6f2be1f009dbe9b8dd8a666fb0c0fe8
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-13 15:43:17 +0300

    add links

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -29,10 +29,18 @@ export const Footer = () => {
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <h3>You can find me here:</h3>
                 <div className={style.footerSquares}>
-                    <div style={linkedinIcon} className={style.square}></div>
-                    <div style={telegramIcon} className={style.square}></div>
-                    <div style={mailIcon} className={style.square}></div>
-                    <div style={githubIcon} className={style.square}></div>
+                    <a target={'_blank'} href="https://www.linkedin.com/in/diana-m-093620225/">
+                        <div style={linkedinIcon} className={style.square}></div>
+                    </a>
+                    <a target={'_blank'} href="https://t.me/maldi2910">
+                        <div style={telegramIcon} className={style.square}></div>
+                    </a>
+                    <a target={'_blank'} href="https://e.mail.ru/cgi-bin/sentmsg?To=malevich_diana15@mail.ru&from=otvet&afterReload=1">
+                        <div style={mailIcon} className={style.square}></div>
+                    </a>
+                    <a target={'_blank'} href="https://github.com/MalevichDi2910">
+                        <div style={githubIcon} className={style.square}></div>
+                    </a>
                 </div>
                 <h2>@ 2022 All rights reserved</h2>
             </div>
