bd914f7d128e5f8dd743376a08585e7786b92cdd
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-13 15:17:52 +0300

    add style in project

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -5,12 +5,12 @@ export const Project = (props) => {
 
     return (
         <div className={style.project}>
-            <div className={style.image} style={props.style}>
-                <a className={style.viewBtn} href={''}>View</a>
-            </div>
+            <div className={style.image} style={props.style}></div>
             <div className={style.projectInfo}>
-                <h3 className={style.projectTitle}>{props.title}</h3>
-                <span className={style.description}><span>{props.description}</span></span>
+                <h3>{props.title}</h3>
+                <div>
+                    <span>{props.description}</span>
+                </div>
             </div>
         </div>
     );
