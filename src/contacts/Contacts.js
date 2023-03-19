44fd0ec534ae730d74d648343c0c052c038cb37b
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-19 17:24:23 +0300

    add component Title

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,13 +1,14 @@
 import React from "react";
 import style from './Contacts.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
+import styleContainer from '../common/styles/Container.module.css';
+import {Title} from "../common/components/title/Title";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <span>Contact</span>
-                <h2 className={style.title}>Contact With Me</h2>
+                <Title text={'Contact With Me'}/>
                 <form className={style.contactsForm}>
                     <div className={style.blockInfo}>
                         <div>
