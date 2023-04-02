649568f57d075a656191ed4ac69b2947cdac97b5
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-02 22:39:59 +0300

    edit Nav

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,14 +1,41 @@
 import React from "react";
 import style from './Nav.module.scss';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export const Nav = () => {
     return (
         <div className={style.nav}>
-            <a href="">HOME</a>
+
+            <Link activeClass={style.active}
+                to="main"
+                smooth={true}
+                offset={1}
+                duration={500}
+            >HOME</Link>
+
             <a href="">ABOUT</a>
-            <a href="">SKILLS</a>
-            <a href="">PROJECTS</a>
-            <a href="">CONTACTS</a>
+
+            <Link activeClass={style.active}
+                  to="skills"
+                  smooth={true}
+                  offset={1}
+                  duration={500}
+            >SKILLS</Link>
+
+            <Link
+                activeClass={style.active}
+                to="projects"
+                smooth={true}
+                offset={1}
+                duration={500}
+            >PROJECTS</Link>
+
+            <Link activeClass={style.active}
+                  to="contacts"
+                  smooth={true}
+                  offset={1}
+                  duration={500}
+            >CONTACTS</Link>
         </div>
     );
 }
