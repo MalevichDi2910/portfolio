73c639c2b6e1ca2e29116f2060b6283352c2e798
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-02 22:38:42 +0300

    add animation

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -5,6 +5,7 @@ import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
 import todoImage from './../assets/image/Guide.jpg';
 import helloImage from './../assets/image/file.jpg';
+import Fade from 'react-reveal/Fade';
 
 export const Projects = () => {
 
@@ -17,12 +18,17 @@ export const Projects = () => {
     };
 
     return (
-        <div className={style.projectsBlock}>
+        <div id='projects' className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <Title text={'Projects'} primaryText={'visit my portfolio and keep your feedback'}/>
+                <Fade bottom>
+                    <Title text={'Projects'} primaryText={'visit my portfolio and keep your feedback'}/>
+                </Fade>
                 <div className={style.projects}>
-                    <Project style={todo} title={'Social network'} description={'Aliquam eros urna, dignissim an '}/>
-                    <Project style={hello} title={'Todolist project'} description={'Aenean at odio dignissim  eros urna.  Lorem ipsum dolor sit amet. '}/>
+                        <Project style={todo} title={'Social network'}
+                                 description={'Aliquam eros urna, dignissim an '}/>
+
+                        <Project style={hello} title={'Todolist project'}
+                                 description={'Aenean at odio dignissim  eros urna.  Lorem ipsum dolor sit amet. '}/>
                 </div>
             </div>
         </div>
