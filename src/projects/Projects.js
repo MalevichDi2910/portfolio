b2eb8d61769dbdb9904837d113c75b11c0ad2ef1
MalevichDi2910 <malevicdiana@gmail.com>
2024-02-19 16:15:55 +0200

    feat: added new images and description

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -3,8 +3,9 @@ import style from "./Projects.module.scss";
 import styleContainer from "../common/styles/Container.module.css";
 import {Project} from "./project/Project";
 import {Title} from "../common/components/title/Title";
-import todoImage from './../assets/image/Guide.jpg';
-import helloImage from './../assets/image/file.jpg';
+import todoImage from './../assets/image/todo.jpg';
+import helloImage from './../assets/image/SN.jpg';
+import cardImage from './../assets/image/card.jpg'
 import Fade from 'react-reveal/Fade';
 
 export const Projects = () => {
@@ -17,6 +18,10 @@ export const Projects = () => {
         backgroundImage: `url(${helloImage})`
     };
 
+    const card = {
+        backgroundImage: `url(${cardImage})`
+    };
+
     return (
         <div id='projects' className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
@@ -24,11 +29,16 @@ export const Projects = () => {
                     <Title text={'Projects'} primaryText={'visit my portfolio and keep your feedback'}/>
                 </Fade>
                 <div className={style.projects}>
-                        <Project style={todo} title={'Social network'}
-                                 description={'Aliquam eros urna, dignissim an '}/>
+                    <Project style={hello} title={'Social network'}
+                             description={'Social Network is an online platform that allows people to create an account and interact with other people on the website.'}/>
 
-                        <Project style={hello} title={'Todolist project'}
-                                 description={'Aenean at odio dignissim  eros urna.  Lorem ipsum dolor sit amet. '}/>
+                    <Project style={card} title={'Flashcards'}
+                             description={'Flashcards is an online platform for creating your own flashcards for learning and memorization.' +
+                                 ' Cards can be for learning foreign words, cards on programming or any other subject or topic.'}/>
+                    <Project style={todo} title={'Task tracker'}
+                             description={'Task tracker is an online platform for organizing your personal' +
+                                 ' or work tasks which can help manage tasks effectively and improve time management.'}/>
+
                 </div>
             </div>
         </div>
