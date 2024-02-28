2fff18900b7be18b2b3d7e6771ea64691ebe758e
MalevichDi2910 <malevicdiana@gmail.com>
2024-02-28 21:21:29 +0200

    feat: add formik and SnackBar

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,53 +1,65 @@
-import React from "react";
 import style from './Contacts.module.scss';
+import {useContactForm} from "./hooks/useContactForm"
 import {Title} from "../common/components/title/Title";
-import Fade from 'react-reveal/Fade';
+import {SnackBar} from "../common/components/snackBar/SnackBar";
 
 export const Contacts = () => {
-    return (
-        <div id='contacts' className={style.contactsBlock}>
-            <div className={style.contactsContainer}>
-                <Fade bottom>
-                <span>Contact</span>
-                <Title text={'Contact With Me'}/>
-                </Fade>
-                <form className={style.contactsForm}>
-                    <div className={style.blockInfo}>
-                            <div>
-                                <label>Your name</label>
-                            </div>
-                            <div>
-                                <input className={style.formArea} type={'text'}/>
-                            </div>
-                    </div>
-                    <div className={style.blockInfo}>
-                            <div>
-                                <label>Phone number</label>
-                            </div>
-                            <div>
-                                <input className={style.formArea} type={'text'}/>
-                            </div>
-                    </div>
-                    <div className={style.blockInfo}>
-                            <div>
-                                <label>Email</label>
-                            </div>
-                            <div>
-                                <input className={style.formArea} type={'email'}/>
-                            </div>
-                    </div>
-                    <div className={style.blockInfo}>
-                            <div>
-                                <label>Your message</label>
-                            </div>
-                            <textarea className={style.messageArea}/>
-                    </div>
-                    <div className={style.blockInfo}>
-                        <button className={style.button}>Send message</button>
-                    </div>
-                </form>
-            </div>
+    const { formik, onDisabled, snackbarShow, snackbarType, snackbarMessage,errors, touched } = useContactForm()
+    const sendEmail = (e) => {
+        e.preventDefault();
+        formik.handleSubmit(e)
+    };
+    return   <div id='contacts' className={style.contactsBlock}>
+        <div className={style.contactsContainer}>
+            <span>Contact</span>
+            <Title text={'Contact With Me'}/>
+            <form className={style.contactsForm} onSubmit={sendEmail}>
+                <div className={style.blockInfo}>
+                    <label>Name</label>
+                    <input
+                        id={"name"}
+                        name={"name"}
+                        className={style.formArea}
+                        value={formik.values.name}
+                        type={"text"}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                {formik.errors.name && formik.touched.name && <div className={style.error}>{formik.errors.name}</div>}
+
+                <div className={style.blockInfo}>
+                    <label>Email</label>
+                    <input
+                        id={"email"}
+                        name={"email"}
+                        className={style.formArea}
+                        value={formik.values.email}
+                        type={"email"}
+                        onChange={formik.handleChange}
+                    />
+                </div>
+                {formik.errors.email && formik.touched.email && <div className={style.error}>{formik.errors.email}</div>}
+
+                <div className={style.blockInfo}>
+                    <label>Message</label>
+                    <textarea
+                        name={"message"}
+                        id={"message"}
+                        value={formik.values.message}
+                        onChange={formik.handleChange}
+                        className={style.messageArea}
+                    />
+                </div>
+                {formik.errors.message && formik.touched.message && <div className={style.error}>{formik.errors.message}</div>}
+
+                <div className={style.blockInfo}>
+                    <button type={"submit"} className={style.button} disabled={onDisabled}>
+                        Send message
+                    </button>
+                </div>
+            </form>
+            <SnackBar show={snackbarShow} type={snackbarType} message={snackbarMessage}/>
         </div>
-    );
+    </div>
 }
 
