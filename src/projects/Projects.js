4d4f9b278f947793b62a81220d9b7d58d8a86309
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-23 19:38:04 +0200

    feat: add link for project

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -29,10 +29,10 @@ export const Projects = () => {
                     <Title text={'Projects'} primaryText={'visit my portfolio and keep your feedback'}/>
 
                 <div className={style.projects}>
-                    <Project style={hello} title={'Social network'} link={'https://MalevichDi2910.github.io/rw_of_samurai'}
+                    <Project style={hello} title={'Social network'} link={'https://MalevichDi2910.github.io/social-network'}
                              description={'Social Network is an online platform that allows people to create an account and interact with other people on the website.'}/>
 
-                    <Project style={card} title={'Flashcards'} link={''}
+                    <Project style={card} title={'Flashcards'} link={'https://flashcards-projectt.vercel.app/sign-in'}
                              description={'Flashcards is an online platform for creating your own flashcards for learning and memorization.' +
                                  ' Cards can be for learning foreign words, programming and etc.'}/>
                     <Project style={todo} title={'Task tracker'} link={'https://MalevichDi2910.github.io/task-tracker'}
